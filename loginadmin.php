3dc94c4e2ee4ab162770872e6f947f4ede3f1f67
Iryna Panchenko <iryna.panchenko@lnu.edu.ua>
2025-03-19 21:27:11 +0200

    Update loginadmin.php

--- loginadmin.php
+++ loginadmin.php
@@ -2,23 +2,23 @@
 // Сесія для збереження статусу входу
 session_start();
 
-// Логін і пароль адміністратора
-$admin_username = 'admin';  
+// Логін і пароль адміністратора (використовуємо email)
+$admin_email = 'admin@example.com';  
 $admin_password = 'password123';  
 
-// Перевірка, чи були введені логін і пароль
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
+// Перевірка, чи були введені email та пароль
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
     // Перевірка введених даних
-    if ($username === $admin_username && $password === $admin_password) {
+    if ($email === $admin_email && $password === $admin_password) {
         // Створення сесії для входу
         $_SESSION['admin_logged_in'] = true;
         header('Location: admin_dashboard.php'); // Перехід на сторінку адміністратора
         exit;
     } else {
-        echo "<p>Невірний логін або пароль!</p>";
+        echo "<p>Невірна пошта або пароль!</p>";
     }
 }
 ?>
